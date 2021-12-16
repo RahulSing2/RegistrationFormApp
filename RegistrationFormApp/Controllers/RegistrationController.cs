cb7adcc99194a8081f06958b304e2da5267f6118
Rahul.Sing2 <Rahul.singh2@ltts.com>
2021-12-16 18:46:36 +0530

    RegistrationFormApp code

---
+++ RegistrationFormApp/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistrationFormApp.Models;
+namespace RegistrationFormApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly ApplicationUser _auc;
+
+        public RegistrationController(ApplicationUser auc)
+        {
+            _auc = auc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(User uc)
+        {
+            _auc.Add(uc);
+            _auc.SaveChanges();
+            
+            ViewBag.message = "The user " + uc.Username + " is saved successfully";
+            return View();
+        }
+    }
+}
